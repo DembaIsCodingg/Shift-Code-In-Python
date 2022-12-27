37ba925df3ec56ea7ca260a5c233bcdd48c61fe9
DembaIsCodingg <121407070+DembaIsCodingg@users.noreply.github.com>
2022-12-27 15:27:53 +0000

    Update Shiftcode.py

--- Shiftcode.py
+++ Shiftcode.py
@@ -1,72 +1,58 @@
 import sys
+
+
 alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v",\
     "w","y","z"]
 
+
+
 def data():
-
-
-    message = input("Enter a message: ")
+    message = input("Enter a word: ")
     message = message.lower()
-    num = int(input("Enter between (1-26): "))
+    num = int(input("Enter a number: "))
     while num >26 or num == 0:
-        num = int(input("Enter between (1-26): "))
+        num = int(input("Enter a number: "))
     all = (message,num)
     return all
 
 def code(message,num):
-    nword = ""
+    newword = ""
     for x in message:
-    
         y = alphabet.index(x)
         y = y + num
         if y > 24:
-            y = y-24
+            y = y - 24
         char = alphabet[y]
-        nword = nword + char
-    print(nword)
-    
+        newword = newword + char
+    print(newword)
+
 def decode(message,num):
-    nword = ""
+    newword = ""
     for x in message:
-        
         y = alphabet.index(x)
         y = y - num
         if y < 0:
             y = y + 24
-        char =alphabet[y]
-        nword = char + nword
-        nword1 = nword[::-1]
-        print(nword1)
-
-  
-    
-        
+        char = alphabet[y]
+        newword = char + newword
+        nword1 = newword[::-1]
+    print(nword1)
 
 def main():
-    again= "y"
+    again = "y"
     while again == "y":
         print("1) Make a code")
-        print("2)Decode a message")
+        print("2) Decde a message")
         print("3)Quit")
-        choose = int(input("Enter your choice: "))
-        if choose == 1:
+        selection = int(input("Enter your selection: "))
+        if selection == 1:
             message,num = data()
             code(message,num)
-        elif choose == 2:
+        elif selection == 2:
             message,num = data()
             decode(message,num)
-        elif choose  == 3:
+        elif selection == 3:
             sys.exit()
-        else:
-            print("Incorrect Selection")
 main()
 
-
-    
-
-    
         
-    
-
-
-
